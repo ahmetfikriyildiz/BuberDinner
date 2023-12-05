435da3089d950d0df7af2c50fac25cbb75dd7835
Ahmet Fikri YILDIZ <ahmetfikriyildiz@gmail.com>
2023-12-06 00:13:09 +0300

    .

--- BuberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
+++ BuberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -12,30 +12,6 @@ namespace BuberDinner.Api.Middleware
     public class ErrorHandlingMiddleware
     {
         private readonly RequestDelegate _next;
-
-        public ErrorHandlingMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public async Task Invoke(HttpContext context)
-        {
-            try
-            {
-                await _next(context);
-            }
-            catch(Exception ex)
-            {
-                await HandleExceptionAsync(context,ex);
-            } 
-        }
-         private static Task HandleExceptionAsync(HttpContext context,Exception exception)
-            {
-                var code= HttpStatusCode.InternalServerError;
-                var result= JsonSerializer.Serialize(new {error="An Error Occurd"});
-                context.Response.ContentType="application/json";
-                context.Response.StatusCode=(int)code;
-                return context.Response.WriteAsync(result);
-            }
+        
     }
 }
